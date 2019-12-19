57ff5229c3f8d649ec1eb318506f7cc9da319954
steve8013 <stevemaxwel@outlook.com>
2019-12-19 16:18:45 +0800

    Build test scripts

--- test.js
+++ test.js
@@ -14,6 +14,35 @@ tape('health', async function (t) {
     t.end()
   })
 })
+tape('put json data', async function (t) {
+  const url = `${endpoint}/rn1abu8/courses/calculus/quizzes/ye0ab61`
+  jsonist.put(url, { score: 98 }, (err, body) => {
+    if (err) t.error(err)
+    t.ok(body['courses'], 'should contain courses in the json data from file')
+    t.ok(body['courses']['calculus'], 'should contain calculus in the body[\'courses\'] from file')
+    t.ok(body['courses']['calculus']['quizzes'], 'should contain quizzes in the body[\'courses\'][\'calculus\'] from file')
+    t.ok(body['courses']['calculus']['quizzes']['ye0ab61'], 'should contain ye0ab61 in the body[\'courses\'][\'calculus\'][\'quizzes\'] from file')
+    t.end()
+  })
+})
+tape('get json data', async function (t) {
+  const url = `${endpoint}/rn1abu8/courses/calculus`
+  jsonist.get(url, (err, body) => {
+    if (err) t.error(err)
+    t.ok(body['quizzes'], 'should contain quizzes in the json data from file')
+    t.ok(body['quizzes']['ye0ab61'], 'should contain ye0ab61 in the body[\'quizzes\'] from file')
+    t.ok(body['quizzes']['ye0ab61']['score'], 'should contain score in the body[\'quizzes\'][\'ye0ab61\'] from file')
+    t.end()
+  })
+})
+tape('delete json data', async function (t) {
+  const url = `${endpoint}/rn1abu8/courses/calculus`
+  jsonist.delete(url, (err, body) => {
+    if (err) t.error(err)
+    t.ok(body['courses'], 'should contain key1 in the json data from file')
+    t.end()
+  })
+})
 
 tape('cleanup', function (t) {
   server.close()
